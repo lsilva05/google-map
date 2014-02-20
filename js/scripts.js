761d09ea2dbaad99bb1d66124a58e8ed850befdd
Silva <lsilva05@engr-131-08.eastern.ewu.edu>
2014-02-20 14:23:36 -0800

    beep

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,18 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
+// Good schtuff
+
+new Maplace({
+	show_markers: true,
+	locations: [{
+		lat: 47.659581, 
+		lon: -117.4236639,
+		zoom: 18
+	}]
+}).Load(); 
+
+
 	console.log('Page Loaded. Lets Do this!');
 
 }); 
